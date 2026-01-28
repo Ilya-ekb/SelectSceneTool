0576aead74dcf39c9ff1d5c79ed0738ae1f60aea
Ilya P <2190055@gmail.com>
2026-01-28 08:29:57 +0300

    First commit

---
+++ Editor/SceneAutoLoader.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+namespace EditorTool.SceneSelectTool
+{
+    [InitializeOnLoad]
+    public static class SceneAutoLoader
+    {
+        static SceneAutoLoader()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeChanged;
+        }
+        private static void OnPlayModeChanged(PlayModeStateChange playModeStateChange)
+        {
+            switch (playModeStateChange)
+            {
+                case PlayModeStateChange.ExitingEditMode:
+                {
+                    var masterSceneExistsInProject = AssetDatabase.AssetPathToGUID(MasterScene) != "";
+
+                    ScenesInHierarchyView = new SceneSetup[] { };
+
+                    if (LoadMasterOnPlay && masterSceneExistsInProject)
+                    {
+                        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                        {
+                            ScenesInHierarchyView = EditorSceneManager.GetSceneManagerSetup();
+
+                            var scene = EditorSceneManager.OpenScene(MasterScene, OpenSceneMode.Single);
+
+                            if (!scene.IsValid())
+                            {
+                                EditorApplication.isPlaying = false;
+                                EditorApplication.update += ReloadLastScene;
+                            }
+                        }
+                        else
+                        {
+                            EditorApplication.isPlaying = false;
+                        }
+                    }
+
+                    break;
+                }
+                case PlayModeStateChange.ExitingPlayMode:
+                {
+                    if (ScenesInHierarchyView.Length != 0)
+                        EditorApplication.update += ReloadLastScene;
+                    break;
+                }
+            }
+        }
+
+        private static void ReloadLastScene()
+        {
+            if (EditorApplication.isPlaying)
+                return;
+            
+            var scenes = ScenesInHierarchyView;
+            var activeSceneIndex = Array.FindIndex(scenes, scene => scene.isActive);
+            var firstLoadedSceneIndex = Array.FindIndex(scenes, scene => scene.isLoaded);
+
+            if (activeSceneIndex != firstLoadedSceneIndex)
+            {
+                scenes[firstLoadedSceneIndex].isActive = true;
+                scenes[activeSceneIndex].isActive = false;
+            }
+            
+            EditorSceneManager.RestoreSceneManagerSetup(scenes);
+            
+            if (activeSceneIndex != firstLoadedSceneIndex)
+                SceneManager.SetActiveScene(SceneManager.GetSceneAt(activeSceneIndex));
+
+
+            ScenesInHierarchyView = new SceneSetup[] { };
+
+            EditorApplication.update -= ReloadLastScene;
+        }
+
+        private static string CEditorPrefLoadMasterOnPlay =>
+            "SceneAutoLoader." + PlayerSettings.productName + ".LoadMasterOnPlay";
+
+        private static string CEditorPrefMasterScene =>
+            "SceneAutoLoader." + PlayerSettings.productName + ".MasterScene";
+
+        private static string CEditorPrefLoadedScenes =>
+            "SceneAutoLoader." + PlayerSettings.productName + ".LoadedScenes";
+
+        public static bool LoadMasterOnPlay
+        {
+            get => EditorPrefs.GetBool(CEditorPrefLoadMasterOnPlay, false);
+            set => EditorPrefs.SetBool(CEditorPrefLoadMasterOnPlay, value);
+        }
+
+        public static string MasterScene
+        {
+            get => EditorPrefs.GetString(CEditorPrefMasterScene, "Master.unity");
+            set => EditorPrefs.SetString(CEditorPrefMasterScene, value);
+        }
+
+        private static SceneSetup[] ScenesInHierarchyView
+        {
+            get
+            {
+                string prefValue = EditorPrefs.GetString(CEditorPrefLoadedScenes, "");
+
+                string[] tokens = prefValue.Split('|');
+
+                int numScenes = tokens.Length / 3;
+
+                SceneSetup[] scenes = new SceneSetup[numScenes];
+
+                for (int i = 0; i < tokens.Length / 3; i++)
+                {
+                    scenes[i] = new SceneSetup();
+                    scenes[i].isActive = (tokens[i * 3 + 0] != "false");
+                    scenes[i].isLoaded = (tokens[i * 3 + 1] != "false");
+                    scenes[i].path = tokens[i * 3 + 2];
+                }
+
+                return scenes;
+            }
+
+            set
+            {
+                string prefValue = string.Join("|",
+                    value.Select(scene =>
+                        (scene.isActive ? "true" : "false") + "|" + (scene.isLoaded ? "true" : "false") + "|" +
+                        scene.path).ToArray());
+
+                EditorPrefs.SetString(CEditorPrefLoadedScenes, prefValue);
+            }
+        }
+    }
+}
---
+++ Editor/SelectScenesTool.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace EditorTool.SceneSelectTool
+{
+    /// <summary>
+    /// A simple little Unity Editor tool for quickly switching between scenes.
+    /// </summary>
+    public class SceneSelectTool : EditorWindow
+    {
+        private Vector2 mScrollPos;
+        private bool mShowAllScenes;
+
+        [MenuItem("Tools/Scene Select Tool")]
+        internal static void Init()
+        {
+            var window = (SceneSelectTool)GetWindow(typeof(SceneSelectTool), false, "Scene Select Tool");
+            window.position = new Rect(window.position.xMin + 100f, window.position.yMin + 100f, 200f, 400f);
+        }
+
+        internal void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            mScrollPos = EditorGUILayout.BeginScrollView(mScrollPos, false, false);
+            GUILayout.Space(10);
+            GUILayout.Label("Settings", EditorStyles.boldLabel);
+            mShowAllScenes = GUILayout.Toggle(mShowAllScenes, "Show All Scenes in the Project");
+            GUILayout.Space(10);
+            
+            var scenesGUIDs = AssetDatabase.FindAssets("t:Scene");
+            var scenesPaths = mShowAllScenes
+                ? scenesGUIDs.Select(AssetDatabase.GUIDToAssetPath).ToArray()
+                : scenesGUIDs.Select(AssetDatabase.GUIDToAssetPath).Where(s => s.StartsWith("Assets/Scenes/"))
+                    .ToArray();
+            GUILayout.Label("Master Loading", EditorStyles.boldLabel);
+
+            if(IsActiveMasterScene())
+                SelectMasterScene(scenesPaths);
+
+            GUILayout.Space(10);
+            
+            GUILayout.Label("Scenes", EditorStyles.boldLabel);
+            DrawScenesList(scenesPaths);
+
+            EditorGUILayout.EndScrollView();
+            EditorGUILayout.EndVertical();
+        }
+
+        private void DrawScenesList(IEnumerable<string> scenesPaths)
+        {
+            var scenesPathsArray = scenesPaths.ToArray();
+            foreach (var path in scenesPathsArray)
+            {
+                var sceneName = Path.GetFileNameWithoutExtension(path);
+                var pressed = GUILayout.Button(sceneName,
+                    new GUIStyle(GUI.skin.GetStyle("Button")) { alignment = TextAnchor.MiddleLeft });
+                if (pressed)
+                    if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                        EditorSceneManager.OpenScene(path);
+            }
+        }
+
+        private static bool IsActiveMasterScene()
+        {
+            var previousLoadMasterOnPlay = SceneAutoLoader.LoadMasterOnPlay;
+
+            var newLoadMasterOnPlay = EditorGUILayout.Toggle("Play From Master Scene", previousLoadMasterOnPlay);
+
+            if (newLoadMasterOnPlay != previousLoadMasterOnPlay)
+                SceneAutoLoader.LoadMasterOnPlay = newLoadMasterOnPlay;
+
+            return newLoadMasterOnPlay;
+        }
+        
+        private void SelectMasterScene(string[] scenePaths)
+        {
+            var previousMasterScene = SceneAutoLoader.MasterScene;
+
+            scenePaths = new[] { string.Empty }.Concat(scenePaths).ToArray();
+            var displayNames = new[] { "<No scene chosen>" }
+                .Concat(scenePaths.Where(s => !string.IsNullOrEmpty(s)).Select(Path.GetFileNameWithoutExtension))
+                .ToArray();
+            
+            var previousSelectedIndex = Math.Max(Array.IndexOf(scenePaths, previousMasterScene), 0);
+            var newSelectedIndex = EditorGUILayout.Popup("Master Scene", previousSelectedIndex, displayNames);
+            
+            var newMasterScene = newSelectedIndex > 0 ? scenePaths[newSelectedIndex] : "";
+
+            if (newMasterScene != previousMasterScene)
+                SceneAutoLoader.MasterScene = newMasterScene;
+        }
+    }
+}
